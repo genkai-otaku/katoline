312b0b5fbfc462b866ab3e679362252f662c43a1
seino914 <tonosaki914@icloud.com>
2026-01-21 03:27:08 +0900

    fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ['selector', '[data-theme="dark"]'],
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: ["selector", '[data-theme="dark"]'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontWeight: {
+        semibold: "700", // font-semiboldを700にマッピング（Noto Sans JPには600がないため）
+      },
+    },
+  },
 };
-
